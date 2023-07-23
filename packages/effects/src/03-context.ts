3af52082cfc73645db50b43a6cc0e529c2beadb6
Sergio L. Benítez D <sergio.kun21@gmail.com>
2023-07-23 12:18:29 -0500

    effect: intro to context.

--- packages/effects/src/03-context.ts
+++ packages/effects/src/03-context.ts
@@ -8,7 +8,24 @@ import { prompt } from "./_utils";
  * =======
  *
  * "Dependency Injection"
+ * 
+ * Previous effects did not have a context; they all had never in the R type.
+ * In this section we are going to start using the R parameter to supply a context.
+ * 
+ * All we need is an interface that defines the service and a Tag that identifies
+ * the service. Think of the context as Map<Tag, Service>.
  *
+ */
+
+/**
+ * To build implementation of service, tags have an "of" method available;
+ * 
+ * i.e. Random.of( {next: () => 42 })
+ * 
+ * Exercise
+ * --------
+ * 
+ * 1. Provide an implementation of the environment and run program1
  */
 
 interface Random {
@@ -28,4 +45,27 @@ const program1 = pipe(
     T.flatMap(random => random.next()),
     T.map(x => x + 1),
     T.provideService(Random, RandomLive)
+)
+
+T.runSync(program1)
+
+// ---
+
+const printLn = (msg: string) => T.sync(() => console.log(msg));
+
+class NonInteractive {
+    readonly _tag = "NonInteractive";
+}
+
+const ask = (message: string) => T.tryCathPromise(
+    async () => {
+        const answer = await prompt(message)
+
+        if (answer === null) {
+            throw undefined;
+        } else {
+            return answer;
+        }
+    },
+    () => new NonInteractive()
 )
