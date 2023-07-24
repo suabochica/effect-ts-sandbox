32912afaa0d867e54be0c525ff0a2458e6a0dcf6
Sergio L. Benítez D <sergio.kun21@gmail.com>
2023-07-24 08:56:05 -0500

    effect: user validation with context.

--- packages/effects/src/03-context.ts
+++ packages/effects/src/03-context.ts
@@ -18,14 +18,8 @@ import { prompt } from "./_utils";
  */
 
 /**
- * To build implementation of service, tags have an "of" method available;
- * 
- * i.e. Random.of( {next: () => 42 })
- * 
- * Exercise
- * --------
- * 
- * 1. Provide an implementation of the environment and run program1
+ * Intro to context w/ random
+ * --------------------------
  */
 
 interface Random {
@@ -40,6 +34,17 @@ const RandomLive = Random.of({
     }
 });
 
+/**
+ * To build implementation of service, tags have an "of" method available;
+ * 
+ * i.e. Random.of( {next: () => 42 })
+ * 
+ * Exercise
+ * --------
+ * 
+ * 1. Provide an implementation of the environment and run program1
+ */
+
 const program1 = pipe(
     Random,
     T.flatMap(random => random.next()),
@@ -49,12 +54,19 @@ const program1 = pipe(
 
 T.runSync(program1)
 
-// ---
+/**
+ * Validate user information
+ * -------------------------
+ */
 
 const printLn = (msg: string) => T.sync(() => console.log(msg));
 
 class NonInteractive {
     readonly _tag = "NonInteractive";
+}
+
+class AuthError {
+    readonly _tag = "AuthError";
 }
 
 const ask = (message: string) => T.tryCathPromise(
@@ -69,3 +81,41 @@ const ask = (message: string) => T.tryCathPromise(
     },
     () => new NonInteractive()
 )
+
+type User = {
+    id: string,
+    userName: string,
+    firstName: string,
+    lastName: string,
+}
+
+interface UserService {
+    login: (
+        username: string,
+        password: string,
+    ) => T.Effect(never, AuthError, User)
+}
+
+const UserService = Context.tag<UserService>();
+
+const program2 = pipe(
+    T.Do(),
+    T.bind("username", () => ask("username: ")),
+    T.bind("password", () => ask("username: ")),
+    T.bind("service", () => UserService),
+    T.flatMap(({ username, password, service }) => service.login(username, password)),
+    T.tap((user) => printLn(`Hellos ${user.firstName}!`))
+)
+
+/**
+ * Exercise
+ * --------
+ * 
+ * 2. Using program2, create a new program to handle error.
+ */
+
+
+/**
+ * Rock, paper scissors (RPS)
+ * -------------------------
+ */
