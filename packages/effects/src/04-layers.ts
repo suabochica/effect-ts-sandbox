2d441e0b0df1e277976796a69e2bc89cbe66f753
Sergio L. Benítez D <sergio.kun21@gmail.com>
2023-07-28 23:40:23 -0500

    effect: intro to layers.

--- packages/effects/src/04-layers.ts
+++ packages/effects/src/04-layers.ts
@@ -26,3 +26,56 @@ import * as L from "@effect/io/Layer"
  * - provide: combine w/o dependency
  * - provideMerge:
  */
+
+type RPSOption = "rock" | "paper" | "scissors";
+
+class NonInteractive {
+    readonly _tag = "NonInteractive";
+}
+
+interface ConsoleService {
+    log: (message: string) => T.Effect<never, never, void>
+}
+
+const ConsoleService = Context.Tag<ConsoleService>();
+
+const ConsoleServiceLive = L.succeed(
+    ConsoleService,
+    ConsoleService.of({
+        log(message) {
+            return T.sync(() => console.log(message))
+        }
+    })
+)
+
+interface IOService {
+    print(message: string) => T.effect<never, never, void>,
+    ask(message: string) => T.effect<never, NonInteractive, void>,
+}
+
+const IOService = Context.Tag<IOService>();
+
+const IOServiceLive = L.effect(
+    IOService,
+    pipe(
+        ConsoleService,
+        T.map((consoleService) => {
+            return IOService.of({
+                aks(message) {
+                    return T.tryCatchPromise(async () => {
+                        const answer = await prompt(message)
+
+                        if (answer === null) {
+                            throw undefined;
+                        } else {
+                            return answer;
+                        }
+                    }, () => new NonInteractive())
+                },
+                print(message) {
+                    return consoleService.log(message)
+                }
+            })
+        })
+    )
+)
