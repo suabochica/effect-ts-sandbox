30787846b91278ae8b268a77d651b9b7e59f05c1
Sergio Benítez <sergio.kun21@gmail.com>
2024-02-26 15:12:51 -0500

    effect: error handling in generators.

--- packages/03-heaviest-pokemon/src/index.gen.ts
+++ packages/03-heaviest-pokemon/src/index.gen.ts
@@ -1,4 +1,4 @@
-import { Effect } from "effect";
+import { Effect, Either } from "effect";
 
 import { getPokemonGen } from "./api/pokemon";
 
@@ -9,10 +9,25 @@ import { getRandomNumberArray, formatPokemon, calculateHeaviestPokemon } from ".
 
 const program =  Effect.gen(function* (_) {
   const arr = yield* _(getRandomNumberArray);
-  const pokemons =  yield* _ (Effect.all(arr.map(getPokemonGen)));
+  const pokemons =  yield* _(Effect.all(arr.map(getPokemonGen)));
   yield* _(Effect.log("\n" + pokemons.map(formatPokemon).join("\n")));
-  const heaviest = yield* _(calculateHeaviestPokemon(pokemons));
-  yield* _(Effect.log(`The heaviest pokemon weighs ${heaviest} hectograms!`));
+  const heaviestResult = yield* _(
+    Effect.either(calculateHeaviestPokemon(pokemons))
+  );
+
+  if (Either.isLeft(heaviestResult)) {
+    yield* _(
+      Effect.log(
+        `Two pokemon have the same weight: ${heaviestResult.left.weight}`
+      )
+    );
+  } else {
+    yield* _(
+      Effect.log(
+        `The heaviest pokemon weighs ${heaviestResult.right} hectograms!`
+      )
+    );
+  }
 });
 
 Effect.runPromise(program);
